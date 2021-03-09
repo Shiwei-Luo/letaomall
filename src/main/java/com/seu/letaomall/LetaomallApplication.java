abf9c82d6b0962fe46e5727b03dac29da44b9030
Shiwei-Luo <1078495372@qq.com>
2021-03-09 13:36:13 +0800

    上传至Git

---
+++ src/main/java/com/seu/letaomall/LetaomallApplication.java
@@ -0,0 +1,13 @@
+package com.seu.letaomall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetaomallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LetaomallApplication.class, args);
+    }
+
+}
